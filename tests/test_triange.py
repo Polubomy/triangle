e64a0d1770cb68c64934d38aab5794bc959446ee
Moonllee <polubomy.max@gmail.com>
2024-06-28 18:14:21 +0300

    2

--- tests/test_triange.py
+++ tests/test_triange.py
@@ -13,7 +13,8 @@ class Rtriangle:
 @pytest.mark.parametrize("data", [
     (3, 2, 3, 6, 1, 2),
     (0, 0, 3, 0, 0, 4),
-    (0, 0, 2, 0, 2, 2)
+    (0, 0, 2, 0, 2, 2),
+    (1, 2, 3, 1, 0, 6)
 ])
 def test_get_right_triangle(data):
 
